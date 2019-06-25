77f3c7850dea7d04ba2817fd6ba3255bc0c007cd
Syed Junaid <sjunaid.uk@gmail.com>
2019-06-25 22:33:33 +0100

    added new inheritance project

---
+++ CarApplication-withInheritance/CarApplication/entitites/Motorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication.entitites
+{
+   public class Motorcycle : Vehicle
+    {
+        
+        public Motorcycle()
+        {
+            Type = "Motorcycle";
+        }
+
+
+
+    }
+}
---
+++ CarApplication-withInheritance/CarApplication/entitites/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication.entitites
+{
+    public class Vehicle
+    {
+        public string Make;
+        public string Model;
+        public string Colour;
+        public string Type;
+
+
+
+
+
+    }
+}
